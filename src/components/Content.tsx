28f294911cc23845ffdb4fb7fb94e208cc574995
Jadson <jadsombruno2016@gmail.com>
2021-03-15 17:50:14 -0300

    Create Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+/**
+ * IMPORTS
+ */
+import { GenreResponseProps, MovieProps } from "../App";
+import { Header } from "./Header";
+import {MovieCard} from "./MovieCard";
+
+
+/**
+ * TYPES
+ */
+interface IContentProps
+{
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+
+/**
+ * EXPORTS
+ */
+export function Content({movies, selectedGenre}: IContentProps) {
+  return (
+    <div className="container">
+        <Header
+          title={selectedGenre.title}
+        />
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
